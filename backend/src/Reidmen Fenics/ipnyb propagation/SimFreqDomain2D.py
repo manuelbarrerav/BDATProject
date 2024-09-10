de1b1c0f6e8699f897af1ef5e96b7074f994eb6a
ManuelB <manuel.barrera@alumnos.uv.cl>
2024-09-10 01:19:41 -0300

    se actualizo la formula de frecuencia

--- backend/src/Reidmen Fenics/ipnyb propagation/SimFreqDomain2D.py
+++ backend/src/Reidmen Fenics/ipnyb propagation/SimFreqDomain2D.py
@@ -405,15 +405,16 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
     tau0 = 0.02
     B = 0.133;   # = 0.04 / 0.3
 
-    tau = 4*(tau0 + B * (por+1)/100)/100
+    tau = (5/10) * (tau0 + B * (por+1)/100)/100
 
     # Iteration over each frequency
     for idx_i in range(nfreq):
 
-        epsilon = (tau)
+        
         ## Define Neumann boundary condition for source 
         freq, freq_0 = float(freqs[idx_i]), 1 # ~ 0.5 [MHz] ---------------------------------------------------------
         # Define general variance
+        epsilon = (tau) * freq * 2 * pi
         sig_freq = 0.6 # sig_time ~ 0.7 [Mhz] 
         exp_R = Source(freq=freq, freq_0=freq_0,
                     sig_freq=sig_freq, osc="cos",
